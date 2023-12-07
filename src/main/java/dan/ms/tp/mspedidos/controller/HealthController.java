fdd086625dfd16a640096eebf8a6e7a02f4cef7e
mart-dominguez <mdomingu@gmail.com>
2023-12-07 19:52:05 -0300

    soporte para eureka

--- src/main/java/dan/ms/tp/mspedidos/controller/HealthController.java
+++ src/main/java/dan/ms/tp/mspedidos/controller/HealthController.java
@@ -9,17 +9,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("api/health")
 public class HealthController {
     
     @GetMapping
-    public ResponseEntity<Map<String,String>> health(){
+    public ResponseEntity<Map<String,String>> health(HttpServletRequest request){
+        // Get the server name
         Map<String,String> res = new LinkedHashMap<>();
+        res.put("serverName",request.getServerName());
         res.put("app","ms-pedidos");
         res.put("status","OK");
         res.put("timestamp",Instant.now().toString());
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            res.put("serverIp",localhost.getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace(); // Handle the exception appropriately in your code
+        }
         return ResponseEntity.ok().body(res);
     }
 }
